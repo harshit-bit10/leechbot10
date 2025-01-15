24a7609511710ef7785120030731e7db897d3702
harshit-bit10 <109902163+harshit-bit10@users.noreply.github.com>
2025-01-15 21:33:18 +0530

    Update jiocine.py

--- jiocine.py
+++ jiocine.py
@@ -5,7 +5,11 @@ import xmltodict
 session = requests.Session()
 
 # Common Headers for Session
-headers = {'authority': 'content-jiovoot.voot.com', 'accept': 'application/json, text/plain, */*', 'accept-language': 'en-US,en;q=0.9', 'origin': 'https://www.jiocinema.com', 'referer': 'https://www.jiocinema.com/', 'sec-ch-ua': '\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"', 'sec-ch-ua-mobile': '?0', 'sec-ch-ua-platform': '\"Windows\"', 'sec-fetch-dest': 'empty', 'sec-fetch-mode': 'cors', 'sec-fetch-site': 'cross-site', 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
+headers = {
+    "Origin": "https://www.jiocinema.com",
+    "Referer": "https://www.jiocinema.com/",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
+}
 
 # Content Type Dir Map
 contentTypeDir = {
