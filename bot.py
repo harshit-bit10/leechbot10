40b6b9026a916b48e0ea784e39fe4d9b10d2f3af
harshit-bit10 <109902163+harshit-bit10@users.noreply.github.com>
2025-01-16 12:51:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -225,8 +225,8 @@ def fetchPlaybackData(content_id, token):
 
     # Make the POST request with the proxy
     r = session.post(playbackUrl, json=playData, headers=playHeaders)
-    
-
+    if r.status_code = 200:
+        return None
 
     result = r.json()
     if not result['data']:
