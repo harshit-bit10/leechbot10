61b1dc2f3578c9c9aa7ff124c8997e47a574837a
harshit-bit10 <109902163+harshit-bit10@users.noreply.github.com>
2025-01-25 22:06:42 +0530

    Update jiocine.py

--- jiocine.py
+++ jiocine.py
@@ -10,15 +10,7 @@ proxy = {'http':'toonrips:rv2006rv@103.172.85.107:501009'}
 # Common Headers for Session
 headers = {
     "Origin": "https://www.jiocinema.com",
-    "Referer": "https://www.jiocinema.com/",
-    "User -Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Connection": "keep-alive",
-    "DNT": "1",  # Do Not Track
-    "Upgrade-Insecure-Requests": "1",
-    "Cache-Control": "no-cache",  # Prevent caching
-    "Pragma": "no-cache"  # Prevent caching
+    "Referer": "https://www.jiocinema.com/"
 }
 
 # Content Type Dir Map
